708a81c323d655f80b7356cd15226f4a1b3abef7
PRATIK A. PARMAR <161212666+Symbol1975@users.noreply.github.com>
2024-10-25 14:21:45 +0530

    React - Components, State, Props

---
+++ React - Components, State, Props/src/App.js
@@ -0,0 +1,19 @@
+// src/App.js
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app-container">
+      <div className="header">
+        <h1>React JS</h1>
+        <img src="https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg" alt="React Logo" className="logo" />
+      </div>
+      <div className="message">
+        <h2>Hello World</h2>
+      </div>
+    </div>
+  );
+}
+
+export default App;
